1dbcbb7f00fbe54878876ba5d5e03c51f13bbf32
Andrew Firsov <a.firsov@twinwingames.com>
2019-02-07 18:28:02 +0200

    Update;

---
+++ src/framework/Application.js
@@ -0,0 +1,22 @@
+export default class Application {
+
+    constructor( model = {} ) {
+
+        this.model = model
+
+        this.init()
+
+    }
+
+
+    //
+    // GET/SET
+    //
+
+    
+
+    init() {
+
+    }
+
+}
