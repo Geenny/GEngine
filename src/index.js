3a12be031543ab822e5f8dd7f52247e7468746a5
afirsov <firsov.andre@gmail.com>
2020-07-28 19:16:17 +0300

    Update 0.3.2;

--- src/index.js
+++ src/index.js
@@ -1,64 +1,77 @@
-// import Main from "./core/main/Main";
-// import MainVO from "./core/main/MainVO";
-// import Net from "./framework/core/dependencies/network/Net";
+// import Application from "./core/application/Application";
+// import ApplicationVO from "./core/application/vo/ApplicationVO";
 
-// const HTMLElement = document.getElementById('ApplicationContainer');
+// const HTMLElement = document.getElementById( 'ApplicationContainer' );
 
-// const CONFIG = require('./core/config/CONFIG');
-// const mainVO = new MainVO( CONFIG.MAIN.main.vo );
-// mainVO.HTMLElement = HTMLElement;
+// const CONFIG = require( './config/CONFIG' );
+// const VO = new ApplicationVO( CONFIG.MAIN.main.vo );
+// VO.HTMLElement = HTMLElement;
 
-// const application = new Main( mainVO );
-// application.init();
-// window.APP = application;
+// function onStart() {
+
+//     const application = applicationStart();
+
+//     document.addEventListener( "click", onClick );
+//     document.addEventListener( "touchend", onClick );
+
+//     function onClick( event ) {
+//         if ( !application.resourceReady ) return;
+//         application.interaction();
+//         document.removeEventListener( "click", onClick );
+//         document.removeEventListener( "touchend", onClick );
+//     }
+
+// }
+
+// function applicationStart() {
+//     if ( window.g_app ) return window.g_app;
+
+//     const application = new Application( VO );
+//     application.init();
+//     window.g_app = application;
+
+//     return application;
+// }
+
+// function applicationRestart() {
+//     if ( window.g_app ) {
+//         window.g_app.destroy();
+//         window.g_app = null;
+//     }
+
+//     return applicationStart();
+// }
+
+// window.onload = function() {
+//     onStart();
+// };
 
 
 
 
-import Application from "./core/application/Application";
-import ApplicationVO from "./core/application/vo/ApplicationVO";
+import GEngineEntry from "./GEngineEntry";
 
-const HTMLElement = document.getElementById( 'ApplicationContainer' );
+const entry = new GEngineEntry();
+entry.start();
 
-const CONFIG = require( './config/CONFIG' );
-const VO = new ApplicationVO( CONFIG.MAIN.main.vo );
-VO.HTMLElement = HTMLElement;
 
-function onStart() {
 
-    const application = applicationStart();
+// import GEngineEntry from "./GEngineEntry";
+// import Log from "./utils/log/Log";
+// import ArrayUtils from "./utils/tech/ArrayUtils";
+// import MethodUtils from "./utils/tech/MethodUtils";
+// import ObjectUtils from "./utils/tech/ObjectUtils";
+// import StringUtils from "./utils/tech/StringUtils";
+// import Application from "./core/application/Application";
+// import ApplicationVO from "./core/application/vo/ApplicationVO";
+// import ApplicationEvent from "./core/application/event/ApplicationEvent";
 
-    document.addEventListener( "click", onClick );
-    document.addEventListener( "touchend", onClick );
-
-    function onClick( event ) {
-        if ( !application.resourceReady ) return;
-        application.interaction();
-        document.removeEventListener( "click", onClick );
-        document.removeEventListener( "touchend", onClick );
-    }
-
-}
-
-function applicationStart() {
-    if ( window.g_app ) return window.g_app;
-
-    const application = new Application( VO );
-    application.init();
-    window.g_app = application;
-
-    return application;
-}
-
-function applicationRestart() {
-    if ( window.g_app ) {
-        window.g_app.destroy();
-        window.g_app = null;
-    }
-
-    return applicationStart();
-}
-
-window.onload = function() {
-    onStart();
-};
+// exports.GEngineEntry = GEngineEntry;
+// exports.Log = Log;
+// exports.ArrayUtils = ArrayUtils;
+// exports.MethodUtils = MethodUtils;
+// exports.ObjectUtils = ObjectUtils;
+// exports.StringUtils = StringUtils;
+// exports.Application = Application;
+// exports.ApplicationVO = ApplicationVO;
+// exports.ApplicationEvent = ApplicationEvent;
